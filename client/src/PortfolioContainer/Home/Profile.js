3aa18c4753b02cc4c6c6897061d56e58c1e1511b
Tamara Sferco <122523521+TamaraDawg@users.noreply.github.com>
2023-08-17 19:19:30 +1000

    contact me button and resume download working button, title and tagline

--- client/src/PortfolioContainer/Home/Profile.js
+++ client/src/PortfolioContainer/Home/Profile.js
@@ -1,11 +1,14 @@
 import React from 'react'
-
+import Typical from 'react-typical'
 export default function Profile() {
     return (
         <div className='profile-container'>
             <div className='profile-parent'>
                 <div className='profile-details'>
                     <div className='colz'>
+                        <div className='colz-icon'>
+
+
                         <a href='https://www.facebook.com/tamara.sferco/'>
                             <i className='fa fa-facebook-square'></i> 
                             </a>
@@ -21,7 +24,54 @@ export default function Profile() {
                         <a href='#'>
                             <i className='fa fa-twitter'></i> 
                             </a>
+                        </div>
                             
+
+                    </div>
+                    <div className='profile-details-name'>
+                        <span className='primary-text'>
+                            {""}
+                            Hello, I'M <span className='highlighted-text'>Tamara</span>
+                        </span>
+                    </div>
+                    <div className='profile-details-role'>
+                        <span className='primary-text'>
+                            {""}
+                            <h1>
+                                {""}
+                                <Typical 
+                                loop={Infinity}
+                                steps={[
+                                    "Enthusiastic Dev ",
+                                    1000,
+                                    "Full Stack Developer",
+                                    1000,
+                                    "Mern Stack Dev",
+                                    1000,
+                                    "Front End Developer",
+                                    1000,
+                                    "Digital Marketing",
+                                    1000,
+                                ]}
+                                />
+                            </h1>
+                            <span className='profile-role-tagline'>
+                                Aspiring Front-End Developer.
+                            </span>
+                        </span>
+                    </div>
+                    <div className='profile-options'>
+                        <button className='btn primary-btn'>
+                            {""}
+                            Contact Me{" "}
+                        </button>
+                        <a href='TAMARA SFERCO RESUME.pdf' download='TAMARA SFERCO RESUME.pdf'>
+                            <button className='btn highlight-btn'> Get Resume</button>
+                        </a>
+                    </div>
+                </div>
+                <div className='profile-picture'>
+                    <div className='profile-picture-background'>
 
                     </div>
                 </div>
