8c3d1306c9dfe68a75f4ffa54f5a7b9dd399546b
Tamara Sferco <122523521+TamaraDawg@users.noreply.github.com>
2023-08-17 19:01:08 +1000

    link icons work, other than yt google n twitter

--- client/src/PortfolioContainer/Home/Profile.js
+++ client/src/PortfolioContainer/Home/Profile.js
@@ -6,13 +6,13 @@ export default function Profile() {
             <div className='profile-parent'>
                 <div className='profile-details'>
                     <div className='colz'>
-                        <a href='#'>
+                        <a href='https://www.facebook.com/tamara.sferco/'>
                             <i className='fa fa-facebook-square'></i> 
                             </a>
                         <a href="#">
                             <i className='fa fa-google-plus-square'></i> 
                             </a>
-                        <a href='#'>
+                        <a href='https://www.instagram.com/tamara.sferco/'>
                             <i className='fa fa-instagram'></i> 
                             </a>
                         <a href='#'>
